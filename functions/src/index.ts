6e39d6c3807d734be270f6d47c2c2aae69cc79df
Majid <mhadaily@gmail.com>
2023-05-22 19:47:49 +0200

    add automatic sending email via firebase extensions and mailgun

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,9 +2,12 @@ import { initializeApp } from 'firebase-admin/app';
 import { Timestamp, getFirestore } from 'firebase-admin/firestore';
 import { onCall } from 'firebase-functions/v2/https';
 import { auth } from 'firebase-functions';
+import { onDocumentWritten } from 'firebase-functions/v2/firestore';
+import { getAuth } from 'firebase-admin/auth';
 
 initializeApp();
 const firestore = getFirestore();
+const adminAuth = getAuth();
 
 exports.submitUserOrder = onCall(async (request) => {
   const userId = request.auth?.uid;
@@ -40,3 +43,32 @@ exports.addRole = auth.user().onCreate(async (user) => {
 
   return Promise.resolve(null);
 });
+
+exports.sendOrderStatusToUser = onDocumentWritten(
+  'users/{userId}/orders/{orderId}',
+  async (event) => {
+    const userId = event.params.userId;
+    const orderId = event.params.orderId;
+
+    // const previousData = event.data?.before.data();
+    const newStatus = event.data?.after.data()?.status;
+
+    if (newStatus) {
+      const user = await adminAuth.getUser(userId);
+      const userEmail = user.email;
+      const emailRef = firestore.collection('emails');
+
+      return emailRef.add({
+        to: userEmail,
+        template: {
+          name: 'order-status',
+          data: {
+            order_id: orderId,
+            order_status: newStatus,
+          },
+        },
+      });
+    }
+    return Promise.resolve(null);
+  }
+);
